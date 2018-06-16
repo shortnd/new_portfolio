8120bbc6fe41a73ea3af3c3802cede16007a2426
shortnd <ococncol@gmail.com>
2018-06-16 18:05:00 -0400

    started a contact page

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,15 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'new_portfolio',
+    title: 'Collin O\'Connell',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.min.css'}
     ]
   },
   /*
---
+++ pages/contact.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h1 class="title">
+      Contact
+    </h1>
+    <div class="content">
+      <form action="" method="post" name="contact" netlify>
+        <input type="hidden" name="form-name" value="contact" />
+        <label class="label" for="name">
+        Name:
+        </label>
+        <input class="input" name="name" id="name" />
+        <label for="email" class="label">
+          Email:
+        </label>
+        <input class="input" name="email" id="email"/>
+        <label class="label" for="Message">
+          Message:
+        </label>
+        <textarea class="textarea" name="message" id="message"></textarea>
+        <input class="button is-primary" type="submit" value="Send Message" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
