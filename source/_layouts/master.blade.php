3526b7a922a06527321816d94867c0851af68653
shortnd <ococncol@gmail.com>
2018-10-07 23:28:34 -0400

    updated site to be jigsaw

---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>
+            @yield('title', 'Collin O\'Connell')
+        </title>
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <style>
+            #skip {
+                left: -64rem;
+                top: 4rem;
+                z-index: 10;
+                transition: all .3s ease;
+            }
+            #skip:focus {
+                left: 4rem;
+            }
+        </style>
+    </head>
+    <body class="flex flex-col h-full">
+        <div id="body" class="flex-1">
+            @yield('body')
+        </div>
+        @include('_partials.footer')
+    </body>
+</html>
---
+++ source/_partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-rouge pin-b">
+    <div class="container mx-auto py-6">
+      <p class="text-gold text-center">
+        &copy; Copyright 2018 <a class="text-bold no-underline text-gold px-2" href="//coconnell.com">coconnell.com</a> - Site built with <a class="text-bold no-underline text-gold px-2" href="//https://jigsaw.tighten.co">Jigsaw</a>
+      </p>
+    </div>
+  </footer>
