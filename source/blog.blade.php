90ecd9898f41da55a6713f645f25f70a4b03267c
shortnd <ococncol@gmail.com>
2018-10-10 23:25:31 -0400

    added some more to the blog page

--- source/blog.blade.php
+++ source/blog.blade.php
@@ -3,10 +3,11 @@
 
 @section('page-title', 'Blog')
 @section('page-subtitle','Coding, Laravel, Vue, and others')
+@section('description', $page->description)
 
 @section('body')
     @forelse ($blog as $post)
-        {{$post->title}}
+        <a href="{{$post->getPath()}}">{{$post->title}}</a>
     @empty
         <h2 class="text-rouge text-center">There are no posts at this time please check back later</h2>
     @endforelse
