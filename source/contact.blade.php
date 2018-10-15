d2e485ec6fe2db764418a03b7f61b87ae1e42985
shortnd <ococncol@gmail.com>
2018-10-15 06:47:20 -0400

    removed recaptia

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -23,7 +23,6 @@
         <label class="mb-2 font-bold block font-lg text-grey-darker w-full" for="message">Message:</label>
         <textarea class="shadow border appearance-none rounded w-full px-2 py-2 leading-tight resize-none focus:outline-none focus:outline-rouge focus:shadow-outline" name="message" id="message" cols="30" rows="10" placeholder="Enter your message here" required></textarea>
       </div>
-      <div class="mb-4" data-netlify-recaptcha></div>
       <div class="mb-6">
         <button class="px-3 py-2 bg-rouge border-rouge text-white font-semibold border rounded hover:bg-transparent hover:text-rouge" type="submit">Submit</button>
         <button class="px-3 py-2 bg-transparent border-red-darker text-red-darker font-semibold border rounded hover:bg-red-darker hover:text-white" type="reset">Reset</button>
