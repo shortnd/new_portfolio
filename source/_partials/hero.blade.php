79e67a985bdbd4545d83a1af1b2f6d49db458f90
shortnd <ococncol@gmail.com>
2019-01-07 10:19:04 -0500

    removed seo from title

--- source/_partials/hero.blade.php
+++ source/_partials/hero.blade.php
@@ -3,7 +3,7 @@
     <h1 class="text-5xl font-bold text-white uppercase">Collin O'Connell</h1>
     <h2 class="text-2xl font-semibold text-grey-lightest uppercase">Full Stack Developer</h2>
     <p class="text-white mt-6 text-xl">
-      Coding, Laravel, Vue, CSS, Accessibility, SEO and <a class="text-gold font-bold" href="//www.detcityfc.com" rel="noreferrer" target="_blank">#DCTID</a>
+      Coding, Laravel, Vue, CSS, Accessibility and <a class="text-gold font-bold" href="//www.detcityfc.com" rel="noreferrer" target="_blank">#DCTID</a>
     </p>
   </div>
 </section>
