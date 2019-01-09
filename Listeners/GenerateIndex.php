605cf86a7e7774f9a2d66ea18117bbb4b5d2239e
shortnd <ococncol@gmail.com>
2019-01-09 09:35:33 -0500

    still trying to fix sitemap

--- Listeners/GenerateIndex.php
+++ Listeners/GenerateIndex.php
@@ -8,6 +8,7 @@ class GenerateIndex
 {
   public function handle(Jigsaw $jigsaw)
   {
+    $file = $jigsaw->getDestinationPath() . '/index.js';
     $blogData = collect($jigsaw->getCollection('blog')->map(function ($page) use ($jigsaw) {
           return [
               'title' => $page->title,
@@ -16,16 +17,17 @@ class GenerateIndex
           ];
       }))->values();
 
+      file_put_contents($file, json_encode($blogData), FILE_APPEND);
+      
       $projectData = collect($jigsaw->getCollection('projects')->map(function ($page) use ($jigsaw) {
           return [
               'title' => $page->title,
               'link' => rightTrimPath($jigsaw->getConfig('baseUrl'))  . $page->getPath(),
               'snippet' => $page->excerpt(),
-          ];
-      })->values());
-
-      file_put_contents($jigsaw->getDestinationPath() . '/index.json', json_encode($blogData));
+            ];
+        })->values());
+        
+        file_put_contents($file, json_encode($projectData), FILE_APPEND);
       
-      file_put_contents($jigsaw->getDestinationPath() . '/index.json', json_encode($projectData));
   }
 }
